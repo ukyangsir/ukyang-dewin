a9aab4a7afecbf52e444791739f1f74f9ee0183a
ukyang <912413803@qq.com>
2022-05-17 18:20:35 +0800

    小修复

---
+++ Neovide/init.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+#**************************************************
+# Author:         ukyang                          *
+# E-mail:         ukyang_ma@163.com               *
+# Date:           2022-05-16        
+# Description:                                    *
+# Copyright 2022 by ukyang.All Rights Reserved    *
+#**************************************************
+
+echo "============================================================"
+echo "==  ==    ==  ==  ==  ==  ==    ==     ==   ==  ========  =="
+echo "==  ==    ==  ===      =  =    =  =    ==== ==  ==        =="
+echo "==  ==    ==  ====      ==    ======   ==  ===  ========  =="
+echo "==  ========  ==  ==    ==   ===  ===  ==   ==  == == ==  =="
+echo "==  ========  ==  ==    ==  ====  ==== ==   ==  ===== ==  =="
+echo "============================================================"
+
+echo "begin>>>>>>>>>>>>>>>>>>>>"
+#获取当前服务器时间，并格式化
+dqtime=$(date "+%Y-%m-%d %H:%M:%S")
+
+#输出当前服务器时间
+echo "datetime: ${dqtime}"
+echo "<<<<<<<<<<<<<<<<<<<<<<end"
+
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt upgrade
+
+sudo apt install -y curl git openssh-server net-tools 
+echo y|sudo apt install -y neovim
+
+ssh-keygen -t rsa -C "ukyang_ma@163.com"
+
+git clone https://github.com/rupa/z.git 
+git clone https://github.com/ukyangsir/ukyang-vimrelated-Windows.git 
+
+echo '. ~/z/z.sh' >> ~/.bashrc
+echo 'alias vim='nvim''>> ~/.bashrc
+echo 'alias vi='nvim''>> ~/.bashrc
+echo 'alias v='nvim''>> ~/.bashrc
+
+mkdir ~/.config/nvim
+cp -r ukyang-vimrelated-Windows/Neovide/* ~/.config/nvim/
+
+source ~/.bashrc
