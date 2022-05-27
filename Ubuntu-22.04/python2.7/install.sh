fb1de9678293a6d6b6aef15057f93bbf04d4ddbd
ukyang <ukyang@ukyang.localdomain>
2022-05-28 02:23:33 +0800

    update: 更新Ubuntu-22.04版本的python相关脚本----2022-05-28 02:22:59

---
+++ Ubuntu-22.04/python2.7/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright (C)2022 By ukyang. All Rights Reserved.
+# Author: ukyang
+# E-mail: ukyang_ma@163.com
+# Date: 2022-05-28
+# Description:
+
+
+echo -e "\e[36m===========================Intall python2.7=============================\e[0m"
+sudo apt install -y python2
+
+echo -e "\e[36m==============================Intall pip2===============================\e[0m"
+sudo apt install -y python-pip 
--- Ubuntu-22.04/python3.9/install.sh
+++ Ubuntu-22.04/python3.9/install.sh
@@ -4,21 +4,29 @@
 # E-mail: ukyang_ma@163.com
 # Date: 2022-05-28
 # Description: 
-# NOTE: 必备脚本5：python3.9 安装脚本
 
 
 # python3.9 related(completing......)
      # python-pip python3-dev python3-setuptools python3-venv \
+
+echo -e "\e[36m======================Intall python3.9 && pip3.9========================\e[0m"
+cd ~
 curl -LO https://www.python.org/ftp/python/3.9.12/Python-3.9.12.tar.xz
 tar -xf Python-3.9.12.tar.xz 
-cd ~/Python-3.9.12.tar.xz
+echo "ukyang"|sudo -S rm -rf Python-3.9.12.tar.xz
+echo ""
+cd ~/Python-3.9.12
 sudo ./configure --enable-optimizations --with-lto --enable-shared
-sudo make -j
+sudo make -j8
+sudo make altinstall
+sudo ln -s /usr/local/lib/libpython3.9.so.1.0 /usr/lib/
+sudo rm -rf ~/Python-3.9.12
 
-
+echo -e "\e[36m==============================Intall pip3===============================\e[0m"
 sudo apt install -y python3-pip 
-pip install requests
 
+echo -e "\e[36m=========================Intall python-related==========================\e[0m"
+pip3 install requests
 
 
 
