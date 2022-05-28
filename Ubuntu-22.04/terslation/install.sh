b0e8d3f33ae7de510ee9810f73fee68ffc94ff1e
ukyang <ukyang@ukyang.localdomain>
2022-05-28 20:55:46 +0800

    update: 更新Ubuntu-22.04版本脚本----2022-05-28 20:55:36

---
+++ Ubuntu-22.04/terslation/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Copyright (C)2022 By ukyang. All Rights Reserved.
+# Author: ukyang
+# E-mail: ukyang_ma@163.com
+# Date: 2022-05-28
+# Description:
+
+
+cd ~/.config/nvim/plugged/Terslation
+sudo make install
+cd ~
