8e47820d5c1a63838def87152464f77105468716
ukyang <ukyang@ukyang>
2022-05-30 10:27:35 +0800

    update: 更新Neovim配置----2022-05-30 10:27:30

--- shells_to_bin/backup.sh
+++ shells_to_bin/backup.sh
@@ -18,12 +18,12 @@ sudo cp -R ~/ukyang-vimrelated-Windows/Neovide/vim_raw/* ~/.config/nvim/vim_raw/
 sudo cp -R ~/ukyang-vimrelated-Windows/Neovide/init.vim ~/.config/nvim/
 
 
-echo "ukyang"|sudo -S cp -R ~/ukyang-vimrelated-Windows/Neovide/lua/* /mnt/c/Users/mayang/AppData/Local/nvim/lua/
-echo ""
-sudo cp -R ~/ukyang-vimrelated-Windows/Neovide/ftplugin/* /mnt/c/Users/mayang/AppData/Local/nvim/ftplugin/
-sudo cp -R ~/ukyang-vimrelated-Windows/Neovide/lint/* /mnt/c/Users/mayang/AppData/Local/nvim/lint/
-sudo cp -R ~/ukyang-vimrelated-Windows/Neovide/sh/* /mnt/c/Users/mayang/AppData/Local/nvim/sh/
-sudo cp -R ~/ukyang-vimrelated-Windows/Neovide/snippet/* /mnt/c/Users/mayang/AppData/Local/nvim/snippet/
-sudo cp -R ~/ukyang-vimrelated-Windows/Neovide/vim_raw/* /mnt/c/Users/mayang/AppData/Local/nvim/vim_raw/
-sudo cp -R ~/ukyang-vimrelated-Windows/Neovide/init.vim /mnt/c/Users/mayang/AppData/Local/nvim/
+# echo "ukyang"|sudo -S cp -R ~/ukyang-vimrelated-Windows/Neovide/lua/* /mnt/c/Users/mayang/AppData/Local/nvim/lua/
+# echo ""
+# sudo cp -R ~/ukyang-vimrelated-Windows/Neovide/ftplugin/* /mnt/c/Users/mayang/AppData/Local/nvim/ftplugin/
+# sudo cp -R ~/ukyang-vimrelated-Windows/Neovide/lint/* /mnt/c/Users/mayang/AppData/Local/nvim/lint/
+# sudo cp -R ~/ukyang-vimrelated-Windows/Neovide/sh/* /mnt/c/Users/mayang/AppData/Local/nvim/sh/
+# sudo cp -R ~/ukyang-vimrelated-Windows/Neovide/snippet/* /mnt/c/Users/mayang/AppData/Local/nvim/snippet/
+# sudo cp -R ~/ukyang-vimrelated-Windows/Neovide/vim_raw/* /mnt/c/Users/mayang/AppData/Local/nvim/vim_raw/
+# sudo cp -R ~/ukyang-vimrelated-Windows/Neovide/init.vim /mnt/c/Users/mayang/AppData/Local/nvim/
 echo -e "\e[36m同步成功\e[0m"
