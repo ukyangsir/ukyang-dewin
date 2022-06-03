0dab87b0a677bbb6b8395cf54438079fbaeec43b
ukyang <43985176+ukyangsir@users.noreply.github.com>
2022-06-03 20:52:43 +0800

    Update 更新node安装脚本

--- Ubuntu-22.04/nodejs/install.sh
+++ Ubuntu-22.04/nodejs/install.sh
@@ -1,6 +1,8 @@
 # nodejs && npm
 cd /usr/local
 echo "ukyang" | sudo -S wget https://npmmirror.com/mirrors/node/v16.15.0/node-v16.15.0-linux-x64.tar.xz
+echo ""
 sudo tar -xf node-v16.15.0-linux-x64.tar.xz
 sudo rm -rf node-v16.15.0-linux-x64.tar.xz
 sudo mv node-v16.15.0-linux-x64 node
+sudo chmod -R 777 node
