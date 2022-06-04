12bf80d0aa9f6567a197d0969c8d7b314b440756
ukyang <ukyang@ukyang>
2022-06-04 19:46:09 +0800

    update: 更新自定义按键映射列表

--- Neovide/lua/plugin_config/which-key.lua
+++ Neovide/lua/plugin_config/which-key.lua
@@ -4,7 +4,32 @@
 -- Date: 2022-06-04
 -- Description:
 
-require("which-key").setup(
+local wk = require("which-key")
+
+wk.register(
+    {
+        ["g"] = {
+            it = {"<cmd>Gitsigns preview_hunk<CR>", "ukyang--git"},
+            s = {":Switch<CR>", "ukyang--switch"},
+            l = {"<cmd>lua require('toggleterm').lazygit_toggle()<CR>", "ukyang--lazygit"}
+        }
+    }
+)
+
+wk.register(
+    {
+        ["s"] = {
+            p = {":sp<CR>", "ukyang--down"},
+            v = {":vs<CR>", "ukyang--right"},
+            c = {"<C-w>c", "ukyang--close this"},
+            o = {"<C-w>o", "ukyang--close other"},
+            hen = {":set nonumber norelativenumber laststatus=0<CR>", "ukyang--shen模式"},
+            en = {":set nu relativenumber laststatus=2<CR>", "ukyang--sen模式"}
+        }
+    }
+)
+
+wk.setup(
     {
         plugins = {
             spelling = {
@@ -12,6 +37,20 @@ require("which-key").setup(
                 enabled = true,
                 suggestions = 20
             }
-        }
+        },
+        window = {
+            border = "single", -- none, single, double, shadow
+            position = "bottom", -- bottom, top
+            margin = {0, 0, 0, 0}, -- extra window margin [top, right, bottom, left]
+            padding = {0, 0, 0, 0}, -- extra window padding [top, right, bottom, left]
+            winblend = 0
+        },
+        layout = {
+            height = {min = 5, max = 5}, -- min and max height of the columns
+            width = {min = 20, max = 50}, -- min and max width of the columns
+            spacing = 3, -- spacing between columns
+            align = "left" -- align columns left, center or right
+        },
+        -- ignore_missing = true
     }
 )
