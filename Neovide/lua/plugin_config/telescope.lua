f7d68a7e99d46f43eeb4566e4b73178f9206a0f1
ukyang <ukyang@ukyang>
2022-06-04 23:52:36 +0800

    style: 代码格式化

--- Neovide/lua/plugin_config/telescope.lua
+++ Neovide/lua/plugin_config/telescope.lua
@@ -4,38 +4,27 @@
 -- Date: 2022-05-22
 -- Description:
 
-
-require('telescope').setup {
-  defaults = {
-    -- Default configuration for telescope goes here:
-    -- config_key = value,
-    mappings = {
-      i = {
-        ["<A-;>"] = require('telescope.actions').move_selection_next,
-        ["<A-p>"] = require('telescope.actions').move_selection_previous,
-        ["<C-e>"] = require('telescope.actions').close,
-        ["<A-s>"] = require('telescope.actions').select_horizontal,
-        ["<A-v>"] = require('telescope.actions').select_vertical,
-        ["<C-c>"] = false,
-        ["<C-x>"] = false,
-        ["<C-v>"] = false,
-      }
-    }
-  },
-  -- layout_strategy = 'vertical',
-  -- layout_config = { height = 0.5 },
-  pickers = {
-    find_files = {
-      -- theme = "dropdown",
-      -- theme = "cursor",
-      -- theme = "ivy",
-    }
-  },
-  extensions = {
-    -- Your extension configuration goes here:
-    -- extension_name = {
-    --   extension_config_key = value,
-    -- }
-    -- please take a look at the readme of the extension you want to configure
-  }
+require("telescope").setup {
+    defaults = {
+        -- Default configuration for telescope goes here:
+        -- config_key = value,
+        mappings = {
+            i = {
+                ["<A-;>"] = require("telescope.actions").move_selection_next,
+                ["<A-p>"] = require("telescope.actions").move_selection_previous,
+                ["<C-e>"] = require("telescope.actions").close,
+                ["<A-s>"] = require("telescope.actions").select_horizontal,
+                ["<A-v>"] = require("telescope.actions").select_vertical,
+                ["<C-c>"] = false,
+                ["<C-x>"] = false,
+                ["<C-v>"] = false
+            }
+        }
+    },
+    -- layout_strategy = 'vertical',
+    -- layout_config = { height = 0.5 },
+    pickers = {
+        find_files = {}
+    },
+    extensions = {}
 }
