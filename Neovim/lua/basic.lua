734d61893ee37250da1b4440c88ae7beceee0f8c
ukyang <ukyang@ukyang>
2022-06-05 22:48:29 +0800

    add: 新增多种语言的代码片段

--- Neovim/lua/basic.lua
+++ Neovim/lua/basic.lua
@@ -113,6 +113,8 @@ vim.o.incsearch = true
 vim.g.background_transparency = true
 -- 指定undotree缓存存放路径
 vim.g.undotree_dir="~/.cache/nvim/undodir"
+-- 指定代码片段存储路径
+vim.g.vsnip_snippet_dir = "~/.config/nvim/snippet"
 
 require("lsp.lspsaga")
 require("lsp.nvim-cmp")
