519405ca32b457c878aba20a13c1e88332d461e3
ukyang <ukyang@ukyang>
2022-06-06 23:35:13 +0800

    add: 新增markdown预览插件必要依赖安装脚本

---
+++ Ubuntu-22.04/markdown/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Copyright (C)2022 By ukyang. All Rights Reserved.
+# Author: ukyang
+# E-mail: ukyang_ma@163.com
+# Date: 2022-06-06
+# Description:
+
+# echo 'ukyang' | sudo -S apt install pandoc
+# npm install -g @compodoc/live-server
+npm -g install instant-markdown-d@next
